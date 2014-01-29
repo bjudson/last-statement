5388068b210ed56df18ec07d41812e8fc7704ad1
Ben Judson <ben@saharagray.com>
2014-01-28 18:03:39 -0600

    refactor scraper

--- laststatement/task/manage.py
+++ laststatement/task/manage.py
@@ -27,11 +27,13 @@ DEATH_ROW_URLS = {
     'received': 'dr_offenders_on_dr.html'
 }
 
-# JPG_INFO_PATH = '%s/%s' % (app.static_folder, 'info_img')
-JPG_INFO_PATH = None
+JPG_INFO_PATH = '%s/%s' % (app.static_folder, 'info_img')
+# JPG_INFO_PATH = None
 
 os.environ['TZ'] = 'America/Chicago'
 time.tzset()
+
+date = datetime.now().strftime('%Y-%m-%d %H:%M')
 
 manager = Manager(app)
 
@@ -57,7 +59,11 @@ def term_map():
 
 @manager.command
 def tweet():
-    """ Look for statements made on this day, tweet teaser + link """
+    """ Look for statements made on this day, tweet teaser + link
+
+        In practice, this is called using a cron job once a day. Cron needs
+        to activate virtual environment before using this command.
+    """
 
     auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
     auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
@@ -81,19 +87,22 @@ def tweet():
                                          e.message[0]['message'],
                                          e.message[0]['code'])
     else:
-        print '%s Notice: No statement for today'
+        print '%s Notice: No statement for today' % date
 
 
-@manager.command
-def scrape():
-    """ Scrape data from TDCJ and save any new records found
-        TODO: Scrape extra offender data. Much of this is in jpg files, so we
-              will not be able to get a complete data set.
-    """
+def get_existing_offenders():
+    """ Get TDCJ IDs of all executed offenders in database"""
 
-    #
-    # Scrape basic info for all executed offenders
-    #
+    existing = db.session.query(Offender.tdcj_id).all()
+
+    if len(existing) > 0:
+        existing = map(list, zip(*existing))[0]
+
+    return existing
+
+
+def scrape_offenders():
+    """ Scrape all offenders, return as dictionary with databse columns """
 
     # Map TDCJ table column headers to database fields
     exec_table_map = OrderedDict([
@@ -109,8 +118,7 @@ def scrape():
         ('offense_county', 'County')
     ])
 
-    print "Grabbing data..."
-
+    # TODO: what if site is not available? try / except
     doc = urllib2.urlopen(DEATH_ROW_URLS['base'] + DEATH_ROW_URLS['executed'])
     table = BeautifulSoup(doc).table
 
@@ -123,15 +131,8 @@ def scrape():
 
         if table_title != map_title:
             # Looks like format changed, log error & exit
-            app.logger.debug(u'Table headers dont match map: %s (map) - %s '
-                             '(table)' % (map_title, table_title))
-
-    print "Comparing to database..."
-
-    existing = db.session.query(Offender.tdcj_id).all()
-
-    if len(existing) > 0:
-        existing = map(list, zip(*existing))[0]
+            print '%s Error: Table headers dont match map: %s (map) - %s '\
+                '(table)' % (date, map_title, table_title)
 
     data = []
     for r in table.find_all('tr'):
@@ -156,85 +157,120 @@ def scrape():
         else:
             data.append(row_data)
 
-    already_imported = 0
-    new_imports = 0
+    return data
 
-    for d in data:
-        if int(d['tdcj_id']) in existing:
-            already_imported += 1
-        else:
+
+def scrape_statement(statement_url):
+    """ Scrape statement and return as HTML """
+    state_doc = urllib2.urlopen(DEATH_ROW_URLS['base'] + statement_url)
+    soup = BeautifulSoup(state_doc)
+
+    try:
+        # The last statement documents have a paragraph containing
+        # the text "Last Statement:" followed by various numbers of
+        # spaces. The statement text is within one or more paragraphs
+        # following this.
+        statement_tags = soup.find(text=re.compile('Last Statement:'
+                                                   '[\s-]*'))\
+            .parent.find_all_next('p')
+    except AttributeError:
+        return False
+        print '%s Error: Unable to get last statement for %s' %\
+            (date, statement_url)
+    else:
+        return ' '.join([str(t) for t in statement_tags])
+
+
+def scrape_info_jpg(url, execution_num):
+    """ Download image & save to designated directory """
+
+    jpg = urllib2.urlopen(DEATH_ROW_URLS['base'] + url)
+    filename = '%s_%s' % (execution_num,
+                          url.split('/')[-1])
+    with open('%s/%s' % (JPG_INFO_PATH, filename), 'wb') as fp:
+        shutil.copyfileobj(jpg, fp)
+        return True
+
+    return False
+
+
+@manager.command
+def scrape():
+    """ Scrape data from TDCJ and save any new records found
+        TODO: Scrape extra offender data. Much of this is in jpg files, so we
+              will not be able to get a complete data set.
+    """
+
+    #
+    # Scrape basic info for all executed offenders
+    #
+
+    existing = get_existing_offenders()
+    all_offenders = scrape_offenders()
+
+    new_imports = 0
+    statement_urls = {}
+    info_urls = {}
+
+    for d in all_offenders:
+        if int(d['tdcj_id']) not in existing:
             offndr = Offender(**d)
             db.session.add(offndr)
             new_imports += 1
 
+            if offndr.statement_url != 'dr_info/no_last_statement.html':
+                statement_urls[offndr.execution_num] = offndr.statement_url
+
+            if '.jpg' in offndr.info_url:
+                info_urls[offndr.execution_num] = offndr.info_url
+
     try:
         db.session.commit()
     except exc:
-        app.logger.debug('Error saving records')
+        print '%s Error: Cannot save records' % date
 
     #
-    # Scrape last statements
+    # Scrape statements for new offenders
     #
-
-    missing_statement = db.session.query(Offender.id, Offender.statement_url).\
-        filter(Offender.last_statement == None)
 
     statements_added = 0
     statements_failed = 0
 
-    for r in missing_statement:
-        off_id, state_url = r
+    for id, url in statement_urls.items():
+        statement_text = scrape_statement(url)
 
-        if state_url != 'dr_info/no_last_statement.html':
-            state_doc = urllib2.urlopen(DEATH_ROW_URLS['base'] + state_url)
-            soup = BeautifulSoup(state_doc)
-
-            try:
-                # The last statement documents have a paragraph containing
-                # the text "Last Statement:" followed by various numbers of
-                # spaces. The statement text is within one or more paragraphs
-                # following this.
-                statement_tags = soup.find(text=re.compile('Last Statement:'
-                                                           '[\s-]*'))\
-                    .parent.find_all_next('p')
-            except AttributeError:
-                statements_failed += 1
-                app.logger.debug('Unable to get last statement for %s' %
-                                 state_url)
-            else:
-                statements_added += 1
-                statement_string = ' '.join([str(t) for t in statement_tags])
-                db.session.query(Offender).filter(Offender.id == off_id).\
-                    update({Offender.last_statement: statement_string})
+        if statement_text is False:
+            statements_failed += 1
+        else:
+            statements_added += 1
+            db.session.query(Offender).filter(Offender.id == id).\
+                update({Offender.last_statement: statement_text})
 
     db.session.commit()
 
-    print "Statements added: %d" % statements_added
+    print "%s Notice: Statements added: %d" % (date, statements_added)
+    print "%s Notice: Statements failed: %d" % (date, statements_failed)
 
     #
     # Save jpg files
     #
 
-    info_jpgs_downloaded = 0
-
     if JPG_INFO_PATH is not None:
+        info_jpgs_downloaded = 0
+
         if os.access(JPG_INFO_PATH, os.W_OK):
-            with_jpg_info = db.session.query(Offender.id,
-                                             Offender.execution_num,
-                                             Offender.info_url).\
-                filter(Offender.info_url.like("%.jpg%"))
-
-            for o in with_jpg_info:
-                jpg = urllib2.urlopen(DEATH_ROW_URLS['base'] + o.info_url)
-                filename = '%i_%s' % (o.execution_num,
-                                      o.info_url.split('/')[-1])
-                with open('%s/%s' % (JPG_INFO_PATH, filename), 'wb') as fp:
-                    shutil.copyfileobj(jpg, fp)
-
-                info_jpgs_downloaded += 1
+            for num, url in info_urls.items():
+                if scrape_info_jpg(url, num):
+                    info_jpgs_downloaded += 1
+                else:
+                    print '%s Error: Failed to download image from: %s' % \
+                        (date, url)
         else:
-            app.logger.debug('Unable to access JPG_INFO_PATH: %s' %
-                             JPG_INFO_PATH)
+            print '%s Error: Unable to access JPG_INFO_PATH: %s' % \
+                (date, JPG_INFO_PATH)
+
+        print "%s Notice: Info images downloaded: %d" % (date,
+                                                         info_jpgs_downloaded)
 
 
 if __name__ == "__main__":
